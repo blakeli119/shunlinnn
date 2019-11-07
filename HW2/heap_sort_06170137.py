016e0ed7ccf99b6474726ec529f97c6d12e78b90
shunlinnn <55729481+shunlinnn@users.noreply.github.com>
2019-11-08 00:52:28 +0800

    Update heap_sort_06170137.py

--- HW2/heap_sort_06170137.py
+++ HW2/heap_sort_06170137.py
@@ -1,1 +1,25 @@
+class solution(object):
+    def heapSort(self,nums):
+        n = len(nums)
+  
+        for i in range(n, -1, -1):
+            self.heapify(nums, n, i) 
+  
+        for i in range(n-1, 0, -1): 
+            nums[i], nums[0] = nums[0], nums[i]   
+            self.heapify(nums, i, 0) 
+    def heapify(self,nums, n, i): 
+        largest = i  
+        l = 2 * i + 1     
+        r = 2 * i + 2     
 
+        if l < n and nums[i] < nums[l]:
+            largest = l 
+  
+        if r < n and nums[largest] < nums[r]: 
+            largest = r 
+
+        if largest != i:
+            nums[i],nums[largest] = nums[largest],nums[i] 
+  
+            self.heapify(nums, n, largest) 
