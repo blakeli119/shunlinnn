7c3894084b53fa7668235ce2facbff8fb037d10f
shunlinnn <55729481+shunlinnn@users.noreply.github.com>
2019-12-20 11:07:05 +0800

    Update BFS_06170137.py

--- HW5/BFS_06170137.py
+++ HW5/BFS_06170137.py
@@ -1,1 +1,40 @@
+from collections import defaultdict 
+ 
+class Graph:
+    def __init__(self): 
+        self.graph = defaultdict(list) 
+ 
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    def BFS(self, s):
+        """
+        :type s: int
+        :rtype: list
+        """
+        queue = [s]
+        answer= []
+        
+        while queue:
+            s=queue.pop(0)
+            answer.append(s)
+            for a in self.graph[s]:
+                    if a not in answer:
+                        queue.append(a)
+        return answer
 
+    def DFS(self, s):
+        """
+        :type s: int
+        :rtype: list
+        """
+        stack = [s]
+        answer = []
+        
+        while stack:
+            s = stack.pop()
+            answer.append(s)
+            for a in self.graph[s]:
+                if a not in answer:
+                        stack.append(a)   
+        return answer
