8b4cb386cec8c7f4f66a8f02a43675afc11eaa4c
shunlinnn <55729481+shunlinnn@users.noreply.github.com>
2019-12-21 23:18:44 +0800

    Update BFS_06170137.py

--- HW5/BFS_06170137.py
+++ HW5/BFS_06170137.py
@@ -1,32 +1,38 @@
 from collections import defaultdict 
- 
+
 class Graph:
+     
     def __init__(self): 
-        self.graph = defaultdict(list) 
+         self.graph = defaultdict(list) 
  
     def addEdge(self,u,v): 
-        self.graph[u].append(v) 
-  
+        self.graph[u].append(v)
+    
+    
     def BFS(self, s):
+        state2 = []
         queue = [s]
-        answer= []
-        
-        while queue:
-            s=queue.pop(0)
-            answer.append(s)
-            for a in self.graph[s]:
-                    if a not in answer:
-                        queue.append(a)
-        return answer
 
-    def DFS(self, s):
+        while queue:
+            s = queue.pop(0)
+            
+            if s not in state2:
+                state2.append(s)
+                neighbours = self.graph[s]
+                
+                for neighbour in neighbours:
+                    queue.append(neighbour)
+        return state2
+
+    def DFS(self, s): 
+        stateb = []
         stack = [s]
-        answer = []
-        
+
         while stack:
-            s = stack.pop()
-            answer.append(s)
-            for a in self.graph[s]:
-                if a not in answer:
-                        stack.append(a)   
-        return answer
+            s = stack.pop(-1)
+            if s not in stateb:
+                stateb.append(s)
+                neighbours = self.graph[s]
+                for neighbour in neighbours:
+                    stack.append(neighbour)
+        return stateb
