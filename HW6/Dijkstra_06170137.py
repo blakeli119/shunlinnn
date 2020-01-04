b559b601eec6be6d897ec334ed545507fd8b4d41
shunlinnn <55729481+shunlinnn@users.noreply.github.com>
2020-01-04 23:20:22 +0800

    Update Dijkstra_06170137.py

--- HW6/Dijkstra_06170137.py
+++ HW6/Dijkstra_06170137.py
@@ -1,6 +1,4 @@
 from collections import defaultdict 
-import heapq
-import math
 
 class Graph(): 
 
@@ -9,89 +7,39 @@ class Graph():
         self.graph = [] 
         self.graph_matrix = [[0 for column in range(vertices)]  
                     for row in range(vertices)] 
-        
+        self.dict = defaultdict(list) 
     def addEdge(self,u,v,w): 
-        self.graph.append([u,v,w])
-        
+        self.dict[w].append([u,v])
     def Dijkstra(self, s): 
-        s = str(s)
-        graph_dict = self.get_graph_dict()
-        if not graph_dict:
-            return
-
-        pri_queue = list()
-        history = set()
-
-        heapq.heappush(pri_queue, (0, s))
-
-        combine = {s: None}
-        distance = self.get_distance(graph_dict, s)
-
-        while len(pri_queue) > 0:
-            pair = heapq.heappop(pri_queue)
-            span = pair[0]
-            vertex = pair[1]
-            history.add(vertex)
-
-            path_list = graph_dict[vertex].keys()
-            for path in path_list:
-                if path not in history:
-                    new_dist = span + graph_dict[vertex][path]
-                    if new_dist < distance[path]:
-                        heapq.heappush(pri_queue, (new_dist, path))
-                        combine[path] = vertex
-                        distance[path] = new_dist
-
-        return distance
-
-    def get_graph_dict(self) -> dict:
-        if len(self.graph) == 0:
-            return
-
-        result = {}
-        for point in range(len(self.graph)):
-            path_list = self.graph[point]
-
-            data = {}
-            for other, span in enumerate(path_list):
-                if other == point or span == 0:
-                    continue
-                data[str(other)] = span
-
-            result[str(point)] = data
-
-        return result
-
-    def get_distance(self, graph_dict: dict, s: str):
-        result = {s: 0}
-        for vertex in graph_dict.keys():
-            if vertex != s:
-                result[vertex] = math.inf
-        return result
-
-    def Kruskal(self):
-        MST = {}
-        for node in range(len(self.pair)):
-            self.weight = self.sortset(self.pair[node][0], self.pair[node][1])
+        answer = {}
+        for num in range(self.V):
+            final = self.graph[num]
+            for i in range(self.V):
+                for each in range(len(final)):
+                    if final[each] != 0:
+                        nextlist = self.graph[each]
+                        for deep in range(len(nextlist)):
+                            if nextlist[deep] != 0:
+                                if final[deep] == 0 or final[deep] > final[each] + nextlist[deep]:
+                                    final[deep] = final[each] + nextlist[deep]
+            self.graph[num] = final
         
-        for edge in range(len(list(self.weight.keys()))):
-            node1 = list(self.weight.keys())[edge][0]
-            node2 = list(self.weight.keys())[edge][1]
-
-            node1_setidx = self.find_index(node1)
-            node2_setidx = self.find_index(node2)
+        for i in range(self.V):
+            self.graph[i][i] = 0
             
-            len_node1_set = len(self.set[node1_setidx])
-            len_node2_set = len(self.set[node2_setidx])           
-            if len_node1_set >= len_node2_set:
-                if node2 not in self.set[node1_setidx]:
-                    self.set[node1_setidx] += self.set[node2_setidx]
-                    self.set[node2_setidx] = []
-                    MST.setdefault(list(self.weight.keys())[edge], list(self.weight.values())[edge])
-            else:
-                if node1 not in self.set[node2_setidx]:
-                    self.set[node2_setidx] += self.set[node1_setidx]
-                    self.set[node1_setidx] = []
-                    MST.setdefault(list(self.weight.keys())[edge], list(self.weight.values())[edge])                    
-        Kruskal_dict = self.transform(MST)
-        return Kruskal_dict
+        for i in range(self.V):
+            answer[str(i)] = self.graph[s][i]
+            
+        return answer
+    def Kruskal(self):
+        answer={}
+        check = [column for column in range(self.V)]  
+        val = sorted(self.dict)
+        for i in val:
+            for f,s in self.dict[i]:
+                if check[f] == check[s]:
+                    pass
+                else:
+                    check = [check[f] if x==check[s] else x for x in check]
+                    answer[str(f)+'-'+str(s)] = i
+        return answer
